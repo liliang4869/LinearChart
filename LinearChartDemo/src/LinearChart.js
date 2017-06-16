c01c336470eee976a495762eee3905fb4fda5477
liliang4869 <liliang4869@163.com>
2017-06-16 08:55:54 +0800

    坐标轴伸缩

--- LinearChartDemo/src/LinearChart.js
+++ LinearChartDemo/src/LinearChart.js
@@ -6,21 +6,23 @@ const {Surface, Shape, Path,Group} = ART;
 var leftAndTop=25;
 export default class LinearChart extends Component{
     componentWillMount(){
-         this.axisLengthY=10*this.props.DivY; this.axisLengthX=10*this.props.DivX;
-          leftAndTop=0.5*(this.props.style.width-this.axisLengthY)
+         this.axisLengthY=10*this.props.DivY*this.props.Ydens; this.axisLengthX=10*this.props.DivX*this.props.Xdens;
+          leftAndTop=0.5*(this.props.style.width-this.axisLengthX)
+          if(leftAndTop<25)leftAndTop=25
         this.axis=new Path().moveTo(leftAndTop,leftAndTop).lineTo(leftAndTop,leftAndTop+this.axisLengthY).lineTo(leftAndTop+this.axisLengthX,leftAndTop+this.axisLengthY);
         this.Ylines=new Array(10);this.Xlines=new Array(10);
         for(let i=1;i<=10;i++)
         {
-            this.Ylines[i-1]=leftAndTop+i*this.props.DivX;
-            this.Xlines[i-1]=leftAndTop+(i-1)*this.props.DivY;
+            this.Ylines[i-1]=leftAndTop+i*this.props.DivX*this.props.Xdens;
+            this.Xlines[i-1]=leftAndTop+(i-1)*this.props.DivY*this.props.Ydens;
         }
         this.ResultLine=new Path();
         let list=this.props.FxList;
-        this.ResultLine.moveTo(leftAndTop+this.props.startX-this.props.ZeroX,leftAndTop+this.axisLengthY-list[0]+this.props.ZeroY);
+        this.ResultLine.moveTo(leftAndTop+this.props.startX*this.props.Xdens-this.props.ZeroX*this.props.Xdens,leftAndTop+this.axisLengthY-list[0]*this.props.Ydens+this.props.ZeroY*this.props.Ydens);
         for(let j=1;j<list.length;j++)
         {
-            this.ResultLine.lineTo(leftAndTop+this.props.startX-this.props.ZeroX+j*this.props.GapX,leftAndTop+this.axisLengthY-list[j]+this.props.ZeroY);
+            this.ResultLine.lineTo(leftAndTop+this.props.startX*this.props.Xdens-this.props.ZeroX*this.props.Xdens+j*this.props.GapX*this.props.Xdens,
+            leftAndTop+this.axisLengthY-list[j]*this.props.Ydens+this.props.ZeroY*this.props.Ydens);
         }
     }
 render(){
@@ -28,28 +30,28 @@ render(){
     let surfaceWidth=this.props.style.width;
     
     return (<View style={[{height:250,width:250,backgroundColor:'white'},this.props.style]}>
-        {
+        {//竖线
             this.Ylines.map((data,index)=>{
                 return <View style={{height:this.axisLengthY,width:1,backgroundColor:'#eeeeee',position:'absolute',left:data,top:leftAndTop}} key={index}/>
             })
             
         }
-         {
+         {//横坐标
             this.Ylines.map((data,index)=>{
-                return <View style={{backgroundColor:'white',position:'absolute',right:this.props.style.width-data-0.5*this.props.DivX,top:leftAndTop+this.axisLengthY}} key={index}>
+                return <View style={{backgroundColor:'white',position:'absolute',right:this.props.style.width-data-0.5*this.props.DivX*this.props.Xdens,top:leftAndTop+this.axisLengthY}} key={index}>
                     <Text style={{fontSize:10}}>{this.props.ZeroX+(index+1)*this.props.DivX}</Text>
                     </View>
             })
             
         }
-        {
+        {//横线
              this.Xlines.map((data,index)=>{
                 return <View style={{height:1,width:this.axisLengthX,backgroundColor:'#eeeeee',position:'absolute',left:leftAndTop,top:data}} key={index}/>
             })
         }
-        {
+        {//纵坐标
             this.Xlines.map((data,index)=>{
-                return <View style={{backgroundColor:'white',position:'absolute',right:leftAndTop+this.axisLengthY,top:data-5}} key={index}>
+                return <View style={{backgroundColor:'white',position:'absolute',right:leftAndTop+this.axisLengthX,top:data-5}} key={index}>
                     <Text style={{fontSize:10}}>{this.props.ZeroY+(10-index)*this.props.DivY}</Text>
                     </View>
             })
@@ -69,12 +71,13 @@ render(){
 }
 LinearChart.defaultProps={
     ZeroY:0,
-    ZeroX:0,//坐标轴起始位置
-    DivX:20,
-    DivY:20,
+    ZeroX:20,//坐标轴起始位置
+    DivX:30,//X轴每格数据量
+    DivY:30,//Y轴每格数据量
+    Xdens:1.1,//>1拉伸X轴 <1压缩X轴  
+    Ydens:1.5,//>1拉伸Y轴 <1压缩Y轴  
     FxList:[5,10,86,32,42,52,45,63,72,86],
     GapX:20,//x数据密度
-    DivGap:20,//刻度单位
     startX:20,//x开始坐标
-    style:{height:400,width:400}
+    style:{height:800,width:400}
 }
